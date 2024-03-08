4350f10e05020b42e034f9d7e1ebdffebbd9f623
N3sca <lopes.a.baltasar@gmail.com>
2024-03-08 14:28:42 +0000

    themes-sync/fix
    JESUS, enough of this... I think is done

--- thejs.js
+++ thejs.js
@@ -1,17 +1,18 @@
 var theme = window.localStorage.getItem('data-theme');
-if(theme) document.documentElement.setAttribute('data-theme', theme);
+if(theme != null){
+    document.documentElement.setAttribute('data-theme', theme)
+}else document.documentElement.setAttribute('data-theme', 'dark');
+
 
 function themes(){
     if(document.querySelector("html").getAttribute("data-theme") == "dark"){
         document.querySelector("html").setAttribute("data-theme", "light");
         window.localStorage.setItem('data-theme', 'light');
-        document.getElementById("themeS").classList.remove("fa-sun");
-        document.getElementById("themeS").classList.add("fa-moon");
+        document.getElementById("themeS").classList.replace("fa-sun", "fa-moon");
     }else{
         document.querySelector("html").setAttribute("data-theme", "dark");
         window.localStorage.setItem('data-theme', 'dark');
-        document.getElementById("themeS").classList.remove("fa-moon");
-        document.getElementById("themeS").classList.add("fa-sun");
+        document.getElementById("themeS").classList.replace("fa-moon", "fa-sun");
     }
 }
 
