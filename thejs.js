1152bc9fd4bb39bcfaab4249d7c37905841a40da
N3sca <lopes.a.baltasar@gmail.com>
2024-03-08 13:12:45 +0000

    New Page/New Theme B
    New button o nav to change Theme
    New Projects Page
    
    also with some restyling on shadow's

--- thejs.js
+++ thejs.js
@@ -5,8 +5,13 @@ function themes(){
     if(document.querySelector("html").getAttribute("data-theme") == "dark"){
         document.querySelector("html").setAttribute("data-theme", "light");
         window.localStorage.setItem('data-theme', 'light');
+        document.getElementById("themeS").classList.remove("fa-moon");
+        document.getElementById("themeS").classList.add("fa-sun");
     }else{
         document.querySelector("html").setAttribute("data-theme", "dark");
         window.localStorage.setItem('data-theme', 'dark');
+        document.getElementById("themeS").classList.remove("fa-sun");
+        document.getElementById("themeS").classList.add("fa-moon");
     }
 }
+
